a813438efacb89a486635d15e110ac6f4811b0c4
Tony Spegel <TonySpegel@googlemail.com>
2021-09-17 11:08:34 +0200

    - use HTMLAnchorElement not HTMLLinkElement - handler to unregister an event once it has been called

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,26 @@
 // Function to add a link to <head> which then gets prefetched
-export const addPrefetchLink = (event: Event) => {
+export const addPrefetchLink = (event: Event): void => {
     if (event.target !== null) {
         const link = document.createElement('link');
-        
-        link.href = (<HTMLLinkElement> event.target).href;
+
+        link.href = (<HTMLAnchorElement>event.target).href;
         link.rel = 'prefetch';
-    
+
         document.head.appendChild(link);
     }
+};
+
+export const addEventListenerOnce = (
+    link: HTMLAnchorElement,
+    events: ReadonlyArray<keyof HTMLElementEventMap>,
+    prefetch: (event: Event) => void,
+) => {
+    console.log('Registering listeners...');
+    events.forEach((event) => link.addEventListener(event, handler));
+
+    const handler = (event: Event) => {
+        console.log('Removing listeners...');
+        events.forEach((event) => link.removeEventListener(event, handler));
+        prefetch(event);
+    };
 };
