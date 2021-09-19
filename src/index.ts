426d7bf1acd7ab0c6d9b191c3faaf7489b12d534
Tony Spegel <tspegel@fc.eah-jena.de>
2021-09-20 01:06:17 +0200

    remove events from links which aren't needed anymore

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-// Function to add a link to <head> which then gets prefetched
-export const addPrefetchLink = (event: Event): void => {
+/**
+ * Add a link to <head> which then gets prefetched
+ */
+const addToHead = (event: Event): void => {
     if (event.target !== null) {
         const link = document.createElement('link');
 
@@ -10,17 +12,36 @@ export const addPrefetchLink = (event: Event): void => {
     }
 };
 
-export const addEventListenerOnce = (
+/**
+ * Handles the process of adding and removing EventListeners.
+ * Once an event has been executed every other registered event will be removed.
+ * 
+ * Example:
+ * User defined list of events:
+ * const event = ['mouseover', 'focus'];
+ * 
+ * User focuses a link which adds it to the <head>-element.
+ * Because of this any other event is not needed anymore and the one event left
+ * gets removed.
+ * 
+ * 
+ * Usage can look like this:
+ * const links = document.querySelectorAll('a');
+ * 
+ * links.forEach((link) => {
+ *     addPrefetchLink(link);
+ * });
+ */
+export const addPrefetchLink = (
     link: HTMLAnchorElement,
-    events: ReadonlyArray<keyof HTMLElementEventMap>,
-    prefetch: (event: Event) => void,
+    userEvents: ReadonlyArray<keyof HTMLElementEventMap> = ['mouseover', 'focus'],
 ) => {
-    console.log('Registering listeners...');
-    events.forEach((event) => link.addEventListener(event, handler));
-
     const handler = (event: Event) => {
-        console.log('Removing listeners...');
-        events.forEach((event) => link.removeEventListener(event, handler));
-        prefetch(event);
+        // Remove listeners
+        userEvents.forEach((event) => link.removeEventListener(event, handler));
+        addToHead(event);
     };
+
+    // Register listeners
+    userEvents.forEach((event) => link.addEventListener(event, handler));
 };
