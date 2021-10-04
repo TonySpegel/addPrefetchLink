bca8e6413b2e410f7731ff237d8fb697c2adaf40
Tony Spegel <TonySpegel@googlemail.com>
2021-10-04 10:38:41 +0200

    data saver and renaming params

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,24 @@
 /**
  * Add a link to <head> which then gets prefetched
  */
-const addToHead = (event: Event): void => {
+const addToHead = (event: Event): Error | void => {
+    if (navigator.connection) {
+        const connection = navigator.connection;
+        /**
+         * Check if a data saver is running
+         */
+        if (connection.saveData) {
+            return new Error('Prefetch is not available when using Data Saver');
+        }
+        /**
+         * Check for slow connections, don't prefetch on 2g or slower.
+         * effectiveType can be: slow-2g, 2g, 3g, or 4g
+         */
+        if (connection.effectiveType.includes('2g')) {
+            return new Error('Prefetch is not available on slow connection');
+        }
+    }
+
     if (event.target !== null) {
         const link = document.createElement('link');
 
@@ -15,33 +32,36 @@ const addToHead = (event: Event): void => {
 /**
  * Handles the process of adding and removing EventListeners.
  * Once an event has been executed every other registered event will be removed.
- * 
+ *
  * Example:
  * User defined list of events:
  * const event = ['mouseover', 'focus'];
- * 
+ *
  * User focuses a link which adds it to the <head>-element.
  * Because of this any other event is not needed anymore and the one event left
  * gets removed.
- * 
- * 
+ *
+ *
  * Usage can look like this:
  * const links = document.querySelectorAll('a');
- * 
+ *
  * links.forEach((link) => {
  *     addPrefetchLink(link);
  * });
  */
 export const addPrefetchLink = (
     link: HTMLAnchorElement,
-    userEvents: ReadonlyArray<keyof HTMLElementEventMap> = ['mouseover', 'focus'],
+    userEvents: ReadonlyArray<keyof HTMLElementEventMap> = [
+        'mouseover',
+        'focus',
+    ],
 ) => {
     const handler = (event: Event) => {
         // Remove listeners
-        userEvents.forEach((event) => link.removeEventListener(event, handler));
+        userEvents.forEach((userEvent) => link.removeEventListener(userEvent, handler));
         addToHead(event);
     };
 
     // Register listeners
-    userEvents.forEach((event) => link.addEventListener(event, handler));
+    userEvents.forEach((userEvent) => link.addEventListener(userEvent, handler));
 };
